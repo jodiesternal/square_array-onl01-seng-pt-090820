838d8991d8ce87f50bdcaf7b99dc5e9f5de227ac
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-15 15:57:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
-# def square_array(array)
-#   square_array = []
-#   array.each do |number|
-#     square_array << number * number
-#
-#   end
-#   return square_array
-# end
+def square_array(array)
+  square_array = []
+  array.each do |number|
+    square_array << number * number
+
+  end
+  return square_array
+end
