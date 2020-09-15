ead0da3b5f867d381ab35f93e0b67c21406252e5
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-15 15:20:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   square_array = ["1","2","3"]
-  square_array.each
-    puts "array #{number} is #{.collect}"
+  square_array.each do |numbers
+    puts "square_array #{numbers} is #{.collect} "
 
 end
