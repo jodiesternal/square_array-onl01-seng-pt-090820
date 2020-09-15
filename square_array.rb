30cd75f68fdeea2713eff7f409c66614fd5b59ce
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-15 15:49:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   square_array = []
   array.each do |number|
-  square_array << "number * number"
+    square_array << "number * number"
 
   end
+  return square_array
 end
